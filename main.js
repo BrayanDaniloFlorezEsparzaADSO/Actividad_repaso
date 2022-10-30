f63cec9ee4e54ffc439794bc0004972e3d549048
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-10-30 17:54:00 -0500

    ejercicio3

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    let hor= document.querySelector('#horas');
-    let lash= document.querySelector('horas');
-    let cont=0;
-
-    addEventListener('submit', (e) => {
-        e.preventDefault();
-        let dataintput=Object.fromEntries(new FormData(e.target))
-        let valh=5208.33
-        let pgh= dataintput.hor
-        pgh*=valh
-        if (cont <2){
-            let tabla=document.querySelector("tbody");
-            tabla.insertAdjacentElement("beforeend",`
-                <tr>
-                    <th>${hor}</th>
-                    <th>${valh} </th>
-                    <th>${pgh} </th>
-                </tr>`);
-            cont++;
-        }
-    })
-   
-
-
+// hacer el ingreso de datos de N y D 
 })
