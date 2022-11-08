8204e27b11e6f15360767157a7e2497d1774701f
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-11-08 11:13:16 -0500

    punto 4

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 addEventListener("DOMContentLoaded", (e)=>{
-let n = document.querySelector("N")
-let res = ""
-let numeros = document.querySelector("#numeros")
-let suma = 0
-let rta= document.querySelector("#resultados")
-    
-numeros.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    let datosEntrada = Object.fromEntries(new FormData(e.target));
-    for (let s = 1; s < datosEntrada.N; s++){
-         if(datosEntrada.N % s == 0){
-            suma+=s
-        }
-    }
-    if(datosEntrada.N == suma){
-        res = "Es perfecto"
-        suma = 0
-    }
-    else{
-        res = "No es perfecto"
-        suma = 0
-    }
-    rta.innerHTML = ""
-    rta.insertAdjacentHTML("beforeend",`
-    <h2> ${datosEntrada.N + ' ' + res} </h2>
-    `);
-    })
-})
+    let vent=document.querySelector('#elint')
+    let cont=1
+    let venta1= document.querySelector('venta1')
+    let venta2= document.querySelector('venta2')
+    let venta3= document.querySelector('venta3')
 
+    vent.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        if (cont<=50) {       
+        
+        let datosEntrada = Object.fromEntries(new FormData(e.target))
+            
+            venta1 = Number(datosEntrada.venta1)
+            venta2 = Number(datosEntrada.venta2)
+            venta3 = Number(datosEntrada.venta3)
+            let vals=1000000
+            let ventas=venta1+venta2+venta3
+            let ventas2=ventas*0.10
+            let ventas1=ventas2+vals
+            // let pgh= valh*hor
+            
+            let tabla=document.querySelector("tbody");
+            tabla.insertAdjacentHTML("beforeend",`
+                <tr>
+                    <td>${cont}</td>
+                    <td>${venta1}</td>
+                    <td>${venta2} </td>
+                    <td>${venta3} </td>
+                    <td>${vals} </td>
+                    <td>${ventas1} </td>
+
+                </tr>
+                `)
+            cont++
+        }
+    })    
+})
